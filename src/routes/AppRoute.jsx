0082b117b6bfb06f2439759a141e8720882de8cd
amitkroutthedev <akrout455@gmail.com>
2022-09-26 07:56:18 +0530

    routes to all pages

---
+++ src/routes/AppRoute.jsx
@@ -0,0 +1,16 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Loading from "../components/UI/Loading";
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+const FoodPage = lazy(() => import("../pages/FoodPage/FoodPage"));
+
+export default function AppRoute() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/food" element={<FoodPage />} />
+      </Routes>
+    </Suspense>
+  );
+}
