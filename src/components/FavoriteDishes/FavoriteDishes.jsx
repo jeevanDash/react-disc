73bfedbe595b344cad40e1002e44bd2f8194f013
amitkroutthedev <akrout455@gmail.com>
2022-09-27 19:10:27 +0530

    ranked dishes list

--- src/components/FavoriteDishes/FavoriteDishes.jsx
+++ src/components/FavoriteDishes/FavoriteDishes.jsx
@@ -3,6 +3,7 @@ import { PollContext } from "../../context/poll-context";
 import DishCard from "../UI/DishCard";
 const FavoriteDishes = () => {
   const { favoriteDishes } = useContext(PollContext);
+
   <div>
     {favoriteDishes.length &&
       favoriteDishes.map((dish) => <DishCard dish={dish} key={dish.id} />)}
